1b5b287fd701b48a7d177b0922712e79564f6883
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2023-01-11 16:09:11 +0100

    Update ready.js
    removed useless comment

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,6 @@ module.exports = {
 	async execute(client) {
         console.info(`[INFO] Bot online - ${client.user.tag} (${client.user.id})`)
         client.user.setStatus('online');
-        //cron.schedule("0 */4 * * *", function() {
         cron.schedule("* */4 * * *", async function() {
             console.info(`[RUNNER] Started sending servers`);
             await sendServers(client)
