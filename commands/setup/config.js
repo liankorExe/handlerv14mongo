8cd2a448bf970bb33cef10c2e96d3f586ea0cce3
Ninjdai <ChickenTendo3@gmail.com>
2023-01-11 14:15:57 +0100

    Added config command

--- commands/setup/config.js
+++ commands/setup/config.js
@@ -1,11 +1,20 @@
-const { SlashCommandBuilder, EmbedBuilder, ModalBuilder, PermissionsBitField } = require('discord.js');
-const missingPermissionError = require('../../functions');
+const { SlashCommandBuilder, EmbedBuilder, ModalBuilder, PermissionsBitField, ChannelType } = require('discord.js');
 
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('config')
 		.setDescription('Configurer le bot')
+        .addSubcommand(subcommand => subcommand
+            .setName("salon")
+            .setDescription("Choisir le salon où je dois envoyer les serveurs")
+            .addChannelOption(option => option
+                .setName("salon")
+                .setDescription("Le salon où envoyer")
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)
+            )
+        )
         .setDefaultMemberPermissions(0x8),
     async execute(interaction, client){
         await interaction.deferReply({ ephemeral: true });
@@ -17,16 +26,34 @@ module.exports = {
                     .setDescription(`Désolé !\nMon système est uniquement disponible sur les serveurs de plus de 200 membres !\nIl s'agit principalement d'une sécurité destinée à éviter le spam de serveurs. Désolé du désagrément !`)
                     .setColor("#85ca62")
             ]
-        })
+        });
+        switch(interaction.options.getSubcommand()){
+            case 'salon':
+                channel = interaction.options.getChannel('salon');
+                if(!channel.permissionsFor(interaction.guild.fetchMe(), checkAdmin = true).has(PermissionsBitField.Flags.SendMessages)) return interaction.editReply({ content: "Je n'ai pas la permission d'écrire dans le salon indiqué !" });
+                if(!channel.permissionsFor(interaction.guild.fetchMe(), checkAdmin = true).has(PermissionsBitField.Flags.CreateInstantInvite)) return interaction.editReply({ content: "Je n'ai pas la permission de créer une invitation dans le salon indiqué !" });
+                if(!channel.members.size >= 150) return interaction.editReply({ content: "Il faut que la majorité de vos utilisateurs aient la permission de voir ce salon !" });
+                const invite = await interaction.guild.invites.create(channel);
+                client.database.server.create({
+                    name: interaction.guild.id,
+                    guildName: interaction.guild.name,
+                    channel: channel.id,
+                    invite: invite.url,
+                    description: interaction.guild.description,
+                    timestamp: Math.floor(new Date().getTime()/1000),
+                });
+                await channel.send({ embeds: [setupEMBED] });
+                await interaction.editReply({ content: "AdShare a bien été configuré !" })
+        }
 
     }
 }
-
-const formMODAL = new ModalBuilder()
-    .setTitle("AdShare - Formulaire d'admission")
-    .setCustomId("config_admissionform")
 
 const noPermEMBED = new EmbedBuilder()
     .setTitle("Erreur !")
     .setDescription("Désolé, mais vous ne possédez pas la permission `Administrateur`, requise pour effectuer cette action.")
     .setColor("#85ca62")
+
+const setupEMBED = new EmbedBuilder()
+    .setTitle("AdShare")
+    .setDescription("AdShare a été configuré pour envoyer les serveurs dans ce salon !\n[Inviter AdShare](https://discord.com/oauth2/authorize?client_id=1061744784886206536&scope=bot&permissions=0)")
