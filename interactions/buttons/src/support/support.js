e37b303312370544a662832850067d70c982f340
Ninjdai <ChickenTendo3@gmail.com>
2023-01-12 16:41:00 +0100

    Upgraded the admission form

--- interactions/buttons/src/support/support.js
+++ interactions/buttons/src/support/support.js
@@ -1,4 +1,4 @@
-const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require("discord.js")
+const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, ChannelType, PermissionFlagsBits, ButtonBuilder, ButtonStyle } = require("discord.js")
 
 module.exports = {
     async execute(interaction, client){
@@ -7,7 +7,73 @@ module.exports = {
                 await interaction.showModal(formMODAL)
                 break;
             case 'editform':
+                if(!interaction.message.embeds[0]) return interaction.showModal(formMODAL)
+                const editformMODAL = new ModalBuilder()
+                    .setCustomId("support_editform")
+                    .setTitle("AdShare - Formulaire de validation")
+                    .addComponents([
+                        new ActionRowBuilder()
+                            .addComponents([
+                                new TextInputBuilder()
+                                    .setCustomId('invite')
+                                    .setLabel('Invitation vers le serveur')
+                                    .setStyle(TextInputStyle.Short)
+                                    .setMinLength(22)
+                                    .setMaxLength(40)
+                                    .setValue(interaction.message.embeds[0].data.url||"")
+                                    .setPlaceholder(`Format: https://discord.gg/invite`)
+                                    .setRequired(true)
+                            ]),
+                        new ActionRowBuilder()
+                            .addComponents([
+                                new TextInputBuilder()
+                                    .setCustomId('presentation')
+                                    .setLabel('Présentation du serveur')
+                                    .setStyle(TextInputStyle.Paragraph)
+                                    .setMinLength(70)
+                                    .setMaxLength(400)
+                                    .setValue(interaction.message.embeds[0].data.description)
+                                    .setPlaceholder(`Une description de votre serveur pour donner envie ! Changeable plus tard !`)
+                                    .setRequired(true),
+                            ]),
+                    ])
                 await interaction.showModal(editformMODAL)
+                break;
+            case 'submitform':
+                await interaction.deferUpdate()
+                const invite = interaction.message.embeds[0].data.url
+                await interaction.guild.channels.create({
+                    name: `admission-${interaction.user.username}`,
+                    type: ChannelType.GuildText,
+                    parent: '1062761382669467758',
+                    permissionOverwrites: [
+                        {
+                            id: interaction.user.id,
+                            allow: [PermissionFlagsBits.ViewChannel],
+                        },
+                        {
+                            id: interaction.guild.id,
+                            deny: [PermissionFlagsBits.ViewChannel],
+                        },
+        
+                    ],
+                    reason: `AdShare - Admission - ${interaction.user.username} (${interaction.user.id})'`,
+                    topic: `Demande d'admission de <@${interaction.user.id}>\nLien: ${invite}\nID: ${interaction.user.id}\nDate de la demande: <t:${Math.floor(Date.now() / 1000)}:f>`
+                }).then(async channel => {
+                    const commandEmbed = new EmbedBuilder()
+                        .setColor(`#7961fd`)
+                        .setTitle("Formulaire d'admission de " + interaction.user.tag)
+                        .setDescription("Un membre du staff viendra étudier votre admission sous peu.");
+                    await channel.send({
+                        embeds: [commandEmbed, new EmbedBuilder(interaction.message.embeds[0].data)],
+                        components: [admissionROW],
+                    });
+                    await interaction.editReply({
+                        content: `Votre demande d'admission a bien été envoyée !\nRendez-vous dans le salon <#${channel.id}> `,
+                        embeds: [],
+                        components: []
+                    });
+                })
                 break;
         }
     }
@@ -26,7 +92,10 @@ const formMODAL = new ModalBuilder()
                     .setMinLength(22)
                     .setMaxLength(40)
                     .setPlaceholder(`Format: https://discord.gg/invite`)
-                    .setRequired(true),
+                    .setRequired(true)
+            ]),
+        new ActionRowBuilder()
+            .addComponents([
                 new TextInputBuilder()
                     .setCustomId('presentation')
                     .setLabel('Présentation du serveur')
@@ -38,4 +107,14 @@ const formMODAL = new ModalBuilder()
             ]),
     ])
 
-const editformMODAL = formMODAL.setCustomId("support_form");
+const admissionROW = new ActionRowBuilder()
+    .addComponents([
+        new ButtonBuilder()
+            .setCustomId('support_admission-confirm')
+            .setLabel('Valider la demande')
+            .setStyle(ButtonStyle.Success),
+        new ButtonBuilder()
+            .setCustomId('support_admission-reject')
+            .setLabel('Rejeter la demande')
+            .setStyle(ButtonStyle.Danger)
+    ])
--- interactions/modals/src/support/support.js
+++ interactions/modals/src/support/support.js
@@ -1,19 +1,29 @@
-const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     async execute(interaction, client){
-        await interaction.deferReply({ ephemeral: true });
         const type = interaction.customId.replace("support_","");
 		switch(type){
             case 'form':
+                await interaction.deferReply({ ephemeral: true });
                 const invite = interaction.fields.getTextInputValue('invite');
                 const presentation = interaction.fields.getTextInputValue('presentation');
                 if (!invite.includes("discord.gg/")) return interaction.editReply({ content: "Lien non valide ! Merci de réessayer en fournissant une invitation conforme." });
                 await interaction.client.fetchInvite(invite)
                 .then(async Sinvite => {
                     if (!Sinvite) return interaction.editReply({ content: "Lien non valide ! Merci de réessayer en fournissant une invitation conforme." });
+                    if(Sinvite.guild.memberCount<=200) return interaction.editReply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle("Erreur !")
+                                .setDescription(`Désolé !\nMon système est uniquement disponible sur les serveurs de plus de 200 membres !\nIl s'agit principalement d'une sécurité destinée à éviter le spam de serveurs. Désolé du désagrément !`)
+                                .setColor("#85ca62")
+                        ]
+                    });
+                    if(!client.guilds.cache.has(Sinvite.guild.id)) return interaction.editReply({ content: `Merci de [m'ajouter](https://discord.com/oauth2/authorize?client_id=1061744784886206536&scope=bot&permissions=0) à ${Sinvite.guild.name} avant de remplir le formulaire !` })
                     const servinfoEMBED = new EmbedBuilder()
                         .setTitle(Sinvite.guild.name)
+                        .setURL(invite)
                         .setDescription(presentation)
                         .setThumbnail(Sinvite.guild.iconURL({ size: 1024 }))
                     await interaction.editReply({
@@ -23,17 +33,28 @@ module.exports = {
                     });
                 }).catch(error => {
                     console.error(error)
-                })
+                    return interaction.editReply({ content: "Lien non valide ! Merci de réessayer en fournissant une invitation conforme.", embeds: [], components: [formoffROW] });
+                });
                 break;
             case 'editform':
+                await interaction.deferUpdate({ ephemeral: true });
                 const editinvite = interaction.fields.getTextInputValue('invite');
                 const editpresentation = interaction.fields.getTextInputValue('presentation');
                 if (!editinvite.includes("discord.gg/")) return interaction.editReply({ content: "Lien non valide ! Merci de réessayer en fournissant une invitation conforme." });
                 await interaction.client.fetchInvite(editinvite)
                 .then(async Sinvite => {
                     if (!Sinvite) return interaction.update({ content: "Lien non valide ! Merci de réessayer en fournissant une invitation conforme." });
+                    if(Sinvite.guild.memberCount<=200) return interaction.update({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle("Erreur !")
+                                .setDescription(`Désolé !\nMon système est uniquement disponible sur les serveurs de plus de 200 membres !\nIl s'agit principalement d'une sécurité destinée à éviter le spam de serveurs. Désolé du désagrément !`)
+                                .setColor("#85ca62")
+                        ]
+                    });
                     const servinfoEMBED = new EmbedBuilder()
                         .setTitle(Sinvite.guild.name)
+                        .setURL(editinvite)
                         .setDescription(editpresentation)
                         .setThumbnail(Sinvite.guild.iconURL({ size: 1024 }))
                     await interaction.editReply({
@@ -42,8 +63,8 @@ module.exports = {
                         components: [formROW],
                     });
                 }).catch(error => {
-                    console.error(error)
-                })
+                    return interaction.editReply({ content: "Lien non valide ! Merci de réessayer en fournissant une invitation conforme.", embeds: [], components: [formoffROW] });
+                });
                 break;
         }
     }
@@ -54,6 +75,12 @@ const submitBUTTON = new ButtonBuilder()
     .setCustomId("support_submitform")
     .setStyle(ButtonStyle.Success)
 
+const submitoffBUTTON = new ButtonBuilder()
+    .setLabel("Confirmer")
+    .setCustomId("support_submitform")
+    .setStyle(ButtonStyle.Success)
+    .setDisabled(true)
+
 const editBUTTON = new ButtonBuilder()
     .setLabel("Modifier")
     .setCustomId("support_editform")
@@ -61,3 +88,5 @@ const editBUTTON = new ButtonBuilder()
 
 const formROW = new ActionRowBuilder()
     .addComponents([submitBUTTON, editBUTTON])
+const formoffROW = new ActionRowBuilder()
+    .addComponents([submitoffBUTTON, editBUTTON])
