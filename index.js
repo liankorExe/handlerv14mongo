629b0b84ca0eeca6d4768310bae90ac1bc05da27
Ninjdai <ChickenTendo3@gmail.com>
2023-02-15 20:46:35 +0100

    Add working handler

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { Client } = require("discord.js")
+const { Client, Collection } = require("discord.js");
+
 const client = new Client({
     intents: [],
 });
 
 const fs = require("fs")
 client.interactionManager = {
-    buttons: buttons,
-    selectMenus: selectMenus,
-    modals: modals,
+    buttons: new Collection(),
+    selectMenus: new Collection(),
+    modals: new Collection(),
 };
 
 module.exports = client;
