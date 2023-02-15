eccc182eaecbcb60d2287afcd6293494620bd693
Ninjdai <ChickenTendo3@gmail.com>
2023-02-15 20:28:11 +0100

    Replace var by const and add missing semicolons

--- handler/menus.js
+++ handler/menus.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const chalk = require('chalk')
-var AsciiTable = require('ascii-table')
-var table = new AsciiTable()
-table.setHeading('Menus', 'Stats').setBorder('|', '=', "0", "0")
+const AsciiTable = require('ascii-table');
+const table = new AsciiTable();
+table.setHeading('Menus', 'Stats').setBorder('|', '=', "0", "0");
 
 module.exports = (client) => {
     fs.readdirSync('./menus/').filter((file) => file.endsWith('.js')).forEach((file) => {
-        const menus = require(`../menus/${file}`)
-        client.interactionManager.menus.set(menus.id, menus)
-        table.addRow(menus.id, '✅')
-    })
-    console.log(chalk.yellow(table.toString()))
+        const menu = require(`../menus/${file}`);
+        client.interactionManager.menus.set(menu.id, menu);
+        table.addRow(menu.id, '✅');
+    });
+    console.log(chalk.yellow(table.toString()));
 };
