2a2de81067ec4226f7787bbaa228ee6da3cb0429
liankorExe <liankoryt@gmail.com>
2023-02-15 19:34:42 +0100

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const { Discord } = require('discord.js');
 const client = require('..');
 const chalk = require('chalk')
 
-client.on('interactionCreate', async interaction => {
+client.on('ready', async interaction => {
     console.log(chalk.red(`${client.user.tag} prêt a pub ! 📌`))
 });
