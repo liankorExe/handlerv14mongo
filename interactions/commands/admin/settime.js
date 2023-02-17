eb2ef94bdd80483a499a6dda54c3fdc91ee530d6
Ninjdai <ChickenTendo3@gmail.com>
2023-02-17 11:14:49 +0100

    Add missing semicolons and move consts

--- interactions/commands/admin/settime.js
+++ interactions/commands/admin/settime.js
@@ -5,30 +5,20 @@ module.exports = {
     name: "settime",
     description: "Choisis le délai d'envoi de votre pub",
     options: [],
-    default_member_permissions: "Administrator",
+    default_member_permissions: 0x8,
     run: async (client, interaction) => {
         let timeData = await timeModel.findOne({ searchInDb: "adshare" })
-        if (!timeData) {
-            await timeModel.create({
-                searchInDb: "adshare",
-                deux: [],
-                quatre: [],
-                six: [],
-                huit: [],
-                douze: [],
-                vingtquatre: []
-            })
-        }
-        timeData = await timeModel.findOne({ searchInDb: "adshare" })
+        if (!timeData) await timeModel.create({
+            searchInDb: "adshare",
+            deux: [],
+            quatre: [],
+            six: [],
+            huit: [],
+            douze: [],
+            vingtquatre: []
+        });
 
-        const hoursMap = {
-            "2H": timeData.deux,
-            "4H": timeData.quatre,
-            "6H": timeData.six,
-            "8H": timeData.huit,
-            "12H": timeData.douze,
-            "24H": timeData.vingtquatre
-        };
+        timeData = await timeModel.findOne({ searchInDb: "adshare" });
 
         const findGuildHour = (guildId) => {
             for (const [hour, guildIds] of Object.entries(hoursMap)) {
@@ -37,46 +27,56 @@ module.exports = {
                 }
             }
             return null;
-        }
+        };
 
         const guildHour = findGuildHour(interaction.guild.id);
-        const selectheuresmenu = new ActionRowBuilder()
-            .addComponents(
-                new StringSelectMenuBuilder()
-                    .setCustomId('selecthours')
-                    .setPlaceholder('Choisis le délai')
-                    .addOptions(
-                        {
-                            label: '2H',
-                            value: 'deux',
-                        },
-                        {
-                            label: '4H',
-                            value: 'quatre',
-                        },
-                        {
-                            label: '6H',
-                            value: 'six',
-                        },
-                        {
-                            label: '8H',
-                            value: 'huit',
-                        },
-                        {
-                            label: '12H',
-                            value: 'douze',
-                        },
-                        {
-                            label: '24H',
-                            value: 'vingtquatre',
-                        },
-                    ),
-            );
         const embedTime = new EmbedBuilder()
             .setTitle(`⌛ Set Time`)
             .setDescription(`\`\`\` \`\`\`\n\n> Voici la liste des heures de pub\nVous êtes actuellement dans la liste des **${guildHour}**\n\n**Pour choisir une heure il vous suffit de choisir votre heure à l'aide du menu déroulant**`)
-            .setColor(process.env.COLOR)
+            .setColor(process.env.COLOR);
 
-        interaction.reply({ embeds: [embedTime], components: [selectheuresmenu], allowedMentions: { parse: [] } })
+        interaction.reply({ embeds: [embedTime], components: [selectheuresmenu], allowedMentions: { parse: [] } });
     }
 };
+
+const hoursMap = {
+    "2H": timeData.deux,
+    "4H": timeData.quatre,
+    "6H": timeData.six,
+    "8H": timeData.huit,
+    "12H": timeData.douze,
+    "24H": timeData.vingtquatre
+};
+
+const selectheuresmenu = new ActionRowBuilder()
+.addComponents(
+    new StringSelectMenuBuilder()
+        .setCustomId('selecthours')
+        .setPlaceholder('Choisis le délai')
+        .addOptions(
+            {
+                label: '2H',
+                value: 'deux',
+            },
+            {
+                label: '4H',
+                value: 'quatre',
+            },
+            {
+                label: '6H',
+                value: 'six',
+            },
+            {
+                label: '8H',
+                value: 'huit',
+            },
+            {
+                label: '12H',
+                value: 'douze',
+            },
+            {
+                label: '24H',
+                value: 'vingtquatre',
+            },
+        ),
+);
