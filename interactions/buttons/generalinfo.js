169ffa44495179b0468af2de349a4fcaf6412d74
Ninjdai <ChickenTendo3@gmail.com>
2023-02-17 15:07:58 +0100

    Changed info message

--- interactions/buttons/generalinfo.js
+++ interactions/buttons/generalinfo.js
@@ -8,7 +8,7 @@ module.exports = {
         const embedInfos = new EmbedBuilder()
             .setTitle(`Infos`)
             .setColor("#2B2D31")
-            .setDescription(`La catégorie général est un système plus performant permettant une meilleur visibilité à votre serveur, votre serveur sera envoyé dans 1 Général de minimum 200 membres et respectant les TOS, toutes les 12H automatiquement. En revanche vous allez recevoir un serveur dans votre général toutes les 12H, cette méthode est du donnant donnant, pour facilité la visibilité pour tous, en effet il comprend des risques pour vous, mais si on ne prend pas de risque on évoluera pas vite .`);
+            .setDescription(`La catégorie générale est un système plus performant, permettant une meilleur visibilité à votre serveur. Votre serveur sera envoyé dans un salon général d'un autre serveur de minimum 200 membres et respectant les TOS, toutes les 12H automatiquement. En retour, un serveur remplissant ces mêmes conditions sera envoyé dans votre général.`);
 
         interaction.reply({ embeds: [embedInfos], ephemeral: true });
     }
