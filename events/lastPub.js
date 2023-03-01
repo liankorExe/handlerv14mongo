5b6b255946e27bc590825c47ac7c1e014db6ca4e
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2023-03-01 20:05:28 +0100

    Replace message deletion catcher

--- events/lastPub.js
+++ events/lastPub.js
@@ -1,21 +1,22 @@
 const client = require('..');
-const { EmbedBuilder, Discord } = require("discord.js")
-const serverModel = require("../schemas/serverSettings")
+const { EmbedBuilder, Discord } = require("discord.js");
+const serverModel = require("../schemas/serverSettings");
 
 client.on('messageDelete', async (message) => {
-    const receiverServerSettings = await serverModel.findOne({ serverID: receivingServersIds[index] });
-    if (!receiverServerSettings.lastMessageUrl == "null") {
-        try {
-            const discordLinkReg = /https?:(?:www\.)?\/\/discord(?:app)?\.com\/channels\/(\d{18})\/(\d{18})\/(\d{18})/g;
-            const [, guildId, channelId, messageId] = discordLinkReg.exec(receiverServerSettings.lastMessageUrl);
-            const oldMsg = await client.channels.cache.get(channelId)?.messages?.fetch(messageId);
-            if (!oldMsg) {
-                console.log(chalk.red(`[SENDER] Last message in ${receiverServerGuild.name} (${receiverServerGuild.id}) was deleted !`));
-                return logchannel.send({ content: `[SENDER] Last message in ${receiverServerGuild.name} (${receiverServerGuild.id}) was deleted !`, components: [receiverBoutonsOptions] });
-            }
-        } catch (error) {
-            console.log(chalk.red(`[SENDER] Last message in ${receiverServerGuild.name} (${receiverServerGuild.id}) was deleted !`));
-            return logchannel.send({ content: `[SENDER] Last message in ${receiverServerGuild.name} (${receiverServerGuild.id}) was deleted !`, components: [receiverBoutonsOptions] });
-        }
-    }
+    const receiverBoutonsOptions = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId(`blacklister_${message?.guild?.id}`)
+                .setLabel(`Blacklister`)
+                .setStyle(ButtonStyle.Danger),
+            new ButtonBuilder()
+                .setCustomId(`supprimer_${message?.guild?.id}`)
+                .setLabel(`Supprimer`)
+                .setStyle(ButtonStyle.Danger),
+        );
+    if(message?.components[0]?.components[0]?.label=='Rejoindre le serveur'){
+        const logchannel = await client.channels.cache.get(process.env.RUNCHANNEL);
+        console.log(chalk.red(`[SENDER] Last message in ${message?.guild?.name} (${message?.guild?.id}) was deleted !`));
+        return logchannel.send({ content: `[SENDER] Last message in ${message?.guild?.name} (${message?.guild?.id}) was deleted !`, components: [receiverBoutonsOptions] });
+    };
 });
