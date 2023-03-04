497175330ba5f3d03694eabe8910c352c2530289
Ninjdai <ChickenTendo3@gmail.com>
2023-03-04 20:39:14 +0100

    Disallow dm slash commands

--- events/slashCommandInteraction.js
+++ events/slashCommandInteraction.js
@@ -1,10 +1,11 @@
 const client = require('..');
 client.on('interactionCreate', async interaction => {
+    if(!interaction.inGuild()) return interaction.reply({ content: "Vous ne pouvez pas executer cette commande en mp !", ephemeral: true });
     const command = client.interactionManager.commands.get(interaction.commandName);
     if (!command) return client.interactionManager.commands.delete(interaction.commandName);
     try {
         await command.run(client, interaction);
     } catch (error) {
         console.log(error);
-    }
+    };
 });
