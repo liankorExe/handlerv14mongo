b148e720c2031cf3d2491644abe4de91386210a7
Ninjdai <ChickenTendo3@gmail.com>
2023-03-05 09:14:20 +0100

    Add uncaught exceptions handling

--- index.js
+++ index.js
@@ -22,4 +22,19 @@ fs.readdirSync('./handler').forEach((handler) => {
     require(`./handler/${handler}`)(client);
 });
 
+// ERROR LOG MANAGEMENT
+// =====================================================================
+let errorLogStream = fs.createWriteStream(__dirname + '/logs/error.log', {
+    flags: 'a'
+});
+// ERROR HANDLING
+// =====================================================================
+process.on('uncaughtException', (err, next) => {
+    var date = new Date();
+    console.error(`+++++++ ${date} error found, logging event +++++++`);
+    console.error(err.stack);
+    errorLogStream.write(`Date: ${date}. Err: ${err.stack}`);
+    return;
+});
+
 client.login(process.env.TOKEN);
