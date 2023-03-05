72899a50e84b309cf925f80d0fa73a1faf871ce2
Ninjdai <ChickenTendo3@gmail.com>
2023-03-05 17:17:25 +0100

    Remove physical logs

--- index.js
+++ index.js
@@ -22,18 +22,12 @@ fs.readdirSync('./handler').forEach((handler) => {
     require(`./handler/${handler}`)(client);
 });
 
-// ERROR LOG MANAGEMENT
-// =====================================================================
-let errorLogStream = fs.createWriteStream(__dirname + '/logs/error.log', {
-    flags: 'a'
-});
 // ERROR HANDLING
 // =====================================================================
 process.on('uncaughtException', (err, next) => {
     var date = new Date();
     console.error(`+++++++ ${date} error found, logging event +++++++`);
     console.error(err.stack);
-    errorLogStream.write(`Date: ${date}. Err: ${err.stack}`);
     return;
 });
 
