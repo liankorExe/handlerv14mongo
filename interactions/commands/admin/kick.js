0beb7bcb1d64762e756646e25bb3ab0cbf0e845c
liankorExe <liankoryt@gmail.com>
2023-06-22 23:47:54 +0200

    commandes ban et kick

--- interactions/commands/admin/kick.js
+++ interactions/commands/admin/kick.js
@@ -1,8 +1,8 @@
 const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
-    name: "ban",
-    description: "Permet de bannir un membre",
+    name: "kick",
+    description: "Permet de kick un membre",
     options: [
         {
             name: "user",
@@ -19,18 +19,19 @@ module.exports = {
     ],
     run: async (client, interaction) => {
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) interaction.reply({ content: `Tu n'as pas la permission d'utiliser cette commande !` })
+        const { options, guild } = interaction
         const member = options.getUser('user')
         const raison = options.getString('raison') || "Aucune raison"
         const user = interaction.user
 
         let members = guild.members.cache.get(member.id)
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.reply({ content: `Vous n'avez pas la perm d'utiliser cette commande !`, ephemeral: true })
-        if (member.id === user.id) return await interaction.reply({ content: `Vous ne pouvez pas vous ban vous même !`, ephemeral: true })
-        if (guild.ownerId === member.id) return await interaction.reply({ content: `Vous ne pouvez pas ban l'owner du serveur !`, ephemeral: true })
-        if (guild.members.me.roles.highest.position <= members.roles.highest.position) return await interaction.reply({ content: `Je ne peux pas ban un utilisateur plus haut grader que moi !`, ephemeral: true })
-        if (interaction.member.roles.highest.position <= members.roles.highest.position) return await interaction.reply({ content: `Vous ne pouvez pas ban un membre plus haut grader que vous !`, ephemeral: true })
+        if (member.id === user.id) return await interaction.reply({ content: `Vous ne pouvez pas vous kick vous même !`, ephemeral: true })
+        if (guild.ownerId === member.id) return await interaction.reply({ content: `Vous ne pouvez pas kick l'owner du serveur !`, ephemeral: true })
+        if (guild.members.me.roles.highest.position <= members.roles.highest.position) return await interaction.reply({ content: `Je ne peux pas kick un utilisateur plus haut grader que moi !`, ephemeral: true })
+        if (interaction.member.roles.highest.position <= members.roles.highest.position) return await interaction.reply({ content: `Vous ne pouvez pas kick un membre plus haut grader que vous !`, ephemeral: true })
 
-        members.ban({ raison })
-        interaction.reply({ content: `Le membre ${members.user.username} vient d'être banni !` })
+        members.kick({ raison })
+        interaction.reply({ content: `Le membre ${members.user.username} vient d'être kick pour la raison suivante : \`${raison}\` !` })
     }
 };
